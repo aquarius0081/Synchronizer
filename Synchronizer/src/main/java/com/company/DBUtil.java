c9aeb493901dde7783601e2f4238ceaf1ea34859
Rinat <player007@kazandom.ru>
2017-03-13 01:16:40 +0300

    Initial commit.

---
+++ Synchronizer/src/main/java/com/company/DBUtil.java
@@ -0,0 +1,40 @@
+package com.company;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DBUtil {
+    public static List<Job> readFromDB() {
+        List<Job> jobs = new ArrayList<Job>();
+        try {
+            DriverManager.registerDriver(new com.microsoft.sqlserver.jdbc.SQLServerDriver());
+            String url = "jdbc:sqlserver://127.0.0.1\\EMPLOYEESLIST:1433;databaseName=Enterprise";
+            String login = "esa";
+            String password = "qweASD12#";
+            Connection con = DriverManager.getConnection(url, login, password);
+            try {
+                Statement stmt = con.createStatement();
+                final ResultSet rs = stmt.executeQuery("SELECT * FROM Job");
+                while (rs.next()) {
+
+                    jobs.add(new Job() {{
+                        setDepcode(rs.getString("DepCode"));
+                        setDepjob(rs.getString("DepJob"));
+                        setDescription(rs.getString("Description"));
+                    }});
+                }
+                rs.close();
+                stmt.close();
+            } finally {
+                con.close();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return jobs;
+    }
+}
---
+++ Synchronizer/src/main/java/com/company/Job.java
@@ -0,0 +1,46 @@
+package com.company;
+
+import javax.xml.bind.annotation.*;
+
+@XmlRootElement
+@XmlType(propOrder = {"depcode", "depjob", "description"})
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Job {
+    @XmlTransient
+    private Long id;
+    private String depcode;
+    private String depjob;
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDepcode() {
+        return depcode;
+    }
+
+    public void setDepcode(String depcode) {
+        this.depcode = depcode;
+    }
+
+    public String getDepjob() {
+        return depjob;
+    }
+
+    public void setDepjob(String depjob) {
+        this.depjob = depjob;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
